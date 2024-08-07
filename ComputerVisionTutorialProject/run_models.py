02a619f0665f17f8d3e9d02bb86902927f382956
omarelmalak28 <omar.elmalak@mail.utoronto.ca>
2024-08-07 15:40:53 -0400

    Added run_models.py tester.

---
+++ ComputerVisionTutorialProject/run_models.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+
+if __name__ == "__main__":
+    model = tf.keras.models.load_model("./trained_models/new_model4.ckpt")
+
+    image = cv2.imread("./media/input_media/radio.png")
+    image = cv2.resize(image, (64, 64))
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    numpy_image = np.array(image_rgb)
+    numpy_image = np.expand_dims(numpy_image, axis=0)
+
+    predictions = model.predict(numpy_image)
+
+    print(predictions)
+
+    predicted_class = np.argmax(predictions, axis=1)
